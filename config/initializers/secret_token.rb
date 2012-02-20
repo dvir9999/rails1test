44abf227929026abf307a29b2ee63fbe472a2868
iDevenv <dvir9999@idevenv.com>
2012-02-20 19:10:41 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0c5c3af7ec09f878301b70eefa3fcdc550ebf8d5703395e0173b0393e6ac53cead55c1fb637d52e806142e0a797421ef5d10321671d704cceb01233378fc8971'
